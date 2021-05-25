9e00d5cf01d3911ddd854094845c30a9456b14d7
Sassan Shokoohi <sassan_shokoohi@me.com>
2021-05-25 01:32:22 -0700

    Updates

---
+++ precache-manifest.680fcc6c65f250b2a08fcde9d410f90c.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0deba80b03a31413391f6cdd1d58fa05",
+    "url": "/index.html"
+  },
+  {
+    "revision": "318032d89b5f36247258",
+    "url": "/static/css/main.f5497eb8.chunk.css"
+  },
+  {
+    "revision": "f6217cbb69c8fe8d8d0a",
+    "url": "/static/js/2.bc60102b.chunk.js"
+  },
+  {
+    "revision": "29eb69a08ea4198f7b46e1db8a3d5045",
+    "url": "/static/js/2.bc60102b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "318032d89b5f36247258",
+    "url": "/static/js/main.e0586023.chunk.js"
+  },
+  {
+    "revision": "f52ba179432c3be28857",
+    "url": "/static/js/runtime-main.554504a0.js"
+  },
+  {
+    "revision": "2a998ae42c8ec577db672f026b0d967a",
+    "url": "/static/media/clb.2a998ae4.png"
+  },
+  {
+    "revision": "cc763176a44a697a3fe851bb40f46745",
+    "url": "/static/media/sassansh.cc763176.png"
+  },
+  {
+    "revision": "e10bf963b94c2432a598f516182ad3c5",
+    "url": "/static/media/sauder.e10bf963.png"
+  },
+  {
+    "revision": "619da0d695131ce950b26e9e96e2adb3",
+    "url": "/static/media/ubc.619da0d6.png"
+  },
+  {
+    "revision": "b65605a59da5b66feb9519d8a514ae5c",
+    "url": "/static/media/ubcpsych.b65605a5.png"
+  },
+  {
+    "revision": "235cbe9f163cc9efb74636dc048d5187",
+    "url": "/static/media/xerusmedical.235cbe9f.png"
+  }
+]);
