52edd8a4edc4839fd5b59c45cdf5efac2c5a5c13
Sassan Shokoohi <sassan_shokoohi@me.com>
2023-02-06 17:03:06 -0800

    Updates to my info ✨

--- src/editable-stuff/config.js
+++ src/editable-stuff/config.js
@@ -58,7 +58,7 @@ const about = {
   imageLink: require("../editable-stuff/sassansh.png"),
   imageSize: 250,
   message:
-    "Hello! My name is Sassan and I recently graduated with a Bachelor of Computer Science degree from the University of British Columbia. I also have a prior Bachelor of Science from UBC in Chemistry, Biology & Environmental Sciences. My love for technology started when I first touched a computer at my dad’s office when I was 7. Fast-forward to today, and I’ve worked on numerous personal projects, developed a healthcare analytics platform for a start-up, and contributed to a student-led mental health web app.",
+    "Hello! My name is Sassan and I am a software engineer with expertise in various programming languages including JavaScript, Python, Java, GoLang, and C/C++. I have hands-on experience working with popular frameworks such as React, Redux, Node, Express, and Vue. Throughout my career, I have worked on a range of projects, from developing personal projects to contributing to a healthcare analytics platform for a start-up and collaborating on a mental health web app. My passion for technology dates back to my childhood, and I am constantly seeking new challenges and opportunities to further my skills in this field.",
   //resume: require("../editable-stuff/Sassan Shokoohi - Resume.pdf"),
 };
 
@@ -179,7 +179,7 @@ const educations = {
   data: [
     {
       degree: "Bachelor of Computer Science",
-      major: "Computer Science",
+      major: "GPA: 86% (3.72/4.33)",
       educationlogo: require("../assets/img/ubc-grey.png"),
       date: "2019 – 2022",
     },
